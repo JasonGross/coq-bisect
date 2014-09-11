c75cd62fc5e610692d6255c8d732627a51cb419c
Jason Gross <jgross@mit.edu>
2014-09-11 16:01:28 -0400

    Update make-environ to be more robust

--- make-environ.sh
+++ make-environ.sh
@@ -3,8 +3,8 @@
 cat > environ <<EOF
 export FILE=example.v
 export ERR_MESSAGE=Error
-export BAD=trunk
-export GOOD=v8.4
+export BAD=origin/trunk
+export GOOD=origin/v8.4
 export CONFIGURE_ARGS="-nodoc -no-native-compiler"
 export MAKE_TARGET=coqlight
 export COQTOP_ARGS="-nois -boot"
